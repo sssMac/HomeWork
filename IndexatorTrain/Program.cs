2612096a011af93bbb83cb0d85def5019aa00de1
Илья Ляпин <IALyapin@stud.kpfu.ru>
2021-03-01 00:21:56 +0300

    add IndexatorTrain project

---
+++ IndexatorTrain/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace IndexatorTrain
+{
+    class Program 
+    {
+        static void Main()
+        {
+
+            string nameTrain;
+
+            DateTime timeS = default; DateTime timeE = default;
+
+
+            //Название поезда
+            Console.Write("Введите название поезда : "); nameTrain = Console.ReadLine();
+
+            //Количество вагонов
+            Console.Write("Количество вагонов : "); int count = CountOfWagon();
+
+            //Место назначение
+            Console.Write("Место назначения : "); string dest = Console.ReadLine();
+
+            //Время отправления и прибытия
+            OperationTime(ref timeS, ref timeE);
+
+            Train train = new Train(dest, nameTrain, timeS, timeE, count);
+
+            train.TrainInfo(train);
+
+        }
+
+        private static void OperationTime(ref DateTime timeS, ref DateTime timeE)
+        {
+            int k = 0;
+            while (k != 2)
+            {
+                if (k == 0)
+                    Console.Write("Время отправления: ");
+                else
+                    Console.Write("Время прибытия: ");
+                string[] txt = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (k == 0)
+                {
+                    timeS = new DateTime(2020, 2, 20, int.Parse(txt[0]), int.Parse(txt[1]), 0);
+                    k++;
+                }
+                else if (k == 1)
+                {
+                    timeE = new DateTime(2020, 2, 21, int.Parse(txt[0]), int.Parse(txt[1]), 0);
+                    k++;
+                }
+
+            }
+        }
+
+        private static int CountOfWagon()
+        {
+            int count;
+            while (true)
+            {
+                try
+                {
+                    count = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch { }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ IndexatorTrain/Train.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IndexatorTrain // Ляпин Илья 11-012
+{
+    class Train
+    {
+        private string Wey { get; set; }
+        private string NameTrain { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeEnd { get; set; }
+        public TimeSpan TimeWey { get; set; }
+
+        public int Wagon { get; set; }
+
+
+        public Train(string wey, string name, DateTime start, DateTime end, int wagon)
+        {
+            Wey = wey;
+            NameTrain = name;
+            TimeStart = start;
+            TimeEnd = end;
+            TimeWey = CalculateTime(TimeStart, TimeEnd);
+            Wagon = wagon;
+        }
+
+        public void TrainInfo(Train train)
+        {
+            Console.Clear();
+            Console.WriteLine("Поезд №" + train.NameTrain + " Вагон(ов) : " + train.Wagon + "\n" +
+                "Пункт назначения : " + train.Wey + "\n" +
+                "Отправление : " + train.TimeStart + "\n" +
+                "Прибытие : " + train.TimeEnd + "\n" +
+                "Время пути : " + train.TimeWey);
+        }
+
+        static TimeSpan CalculateTime(DateTime ts, DateTime te)
+        {
+            TimeSpan interval = te - ts;
+
+            return interval;
+        }
+
+    }
+}
