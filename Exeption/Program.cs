49869b6bbfcfd14c50fe9adc37e800c7085c5d75
Илья Ляпин <IALyapin@stud.kpfu.ru>
2021-03-01 20:04:19 +0300

    Add exeption project

---
+++ Exeption/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Exeption
+{
+    class PersonExeption : Exception
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleMethod(0, 1);
+
+            int[] array = new int[10];
+            //SimpleMethod(array);
+
+            int a = 0;
+            SimpleMethod(a);
+
+            SimpleMethod();
+
+            PersonExeption("коля");
+        }
+
+        static void SimpleMethod(int x, int y)
+        {
+
+            try
+            {
+                Console.WriteLine(y / x);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка: Деление на ноль");
+            }
+        }
+
+        static void SimpleMethod(int a)
+        {
+            try
+            {
+                a = int.Parse(null);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Аргумент null");
+            }
+        }
+
+        static void SimpleMethod(int[] array)
+        {
+            array[11] = 0;
+        }
+
+        static void SimpleMethod()
+        {
+            object value = 12;
+            string s = (string)value;
+        }
+
+        static void PersonExeption(string name)
+        {
+            try
+            {
+                try
+                {
+                    if (Char.IsLetter(name[0]))
+                        throw new Exception();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Имя с маленькой буквы");
+            }
+
+        }
+    }
+}
