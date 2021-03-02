b6bce82797b1512b01d1d0fb2e43b04308bf411c
Илья Ляпин <IALyapin@stud.kpfu.ru>
2021-03-02 14:05:19 +0300

    Add indexator project

---
+++ Indexator/BaseClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexator // Ляпин Илья 11-012
+{
+    class BaseClass
+    {
+        private int[,] data { get; set; }
+
+        public BaseClass()
+        {
+            data = new int[3, 3];
+        }
+
+
+
+        public int this[int index1, int index2]
+        {
+            get
+            {
+                return data[index1, index2];
+            }
+            set
+            {
+                data[index1, index2] = value;
+            }
+
+        }
+
+
+
+
+    }
+}
