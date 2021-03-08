ccf2b327adc45055fb007c4d9580de8e28ea3e68
Илья Ляпин <IALyapin@stud.kpfu.ru>
2021-03-08 23:11:45 +0300

    Add AnonimusMethod project

---
+++ AnonimusMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AnonimusMethod
+{
+    class Program
+    {
+        delegate int MyDelegate2();
+        delegate double MyDelegate1(MyDelegate2[] a);
+        static void Main()
+        {
+            int n = 6;
+
+            MyDelegate1 myDelegate;
+
+            myDelegate = delegate(MyDelegate2[] a) 
+            {
+                double sum = 0;
+
+                foreach (var i in a)
+                {
+                    sum += i.Invoke();
+                }
+
+                return sum / a.Length;
+            };
+
+            var array = new MyDelegate2[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = AddNumber;
+            }
+
+            Console.WriteLine(myDelegate.Invoke(array));
+        }
+
+        static int AddNumber()
+        {
+            return new Random().Next(1, 100);
+        }
+
+    }
+}
