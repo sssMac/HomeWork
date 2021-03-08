0c7f301d898d393dda08c3dfc51d3698e8a3e86b
Илья Ляпин <IALyapin@stud.kpfu.ru>
2021-03-08 22:14:49 +0300

    Add Delegate project

---
+++ Delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate double Calculation(double r);
+        static void Main()
+        {
+            double r = 4;
+            Calculation D = GetCircle;
+            Calculation S = GetCircleArea;
+            Calculation V = GetBallVolume;
+            Console.WriteLine("D = " + D(r)+ "\n" + "S = " + S(r) + "\n" + "V = " + V(r));
+            
+        }
+
+        static double GetCircle(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        static double GetCircleArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        static double GetBallVolume(double r)
+        {
+            return (4 / 3) * Math.PI * r * r * r;
+        }
+    }
+}
