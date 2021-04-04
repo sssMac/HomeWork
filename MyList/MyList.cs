0026e4ebd033d2cc6296aa776ec43df99709bd88
Илья Ляпин <IALyapin@stud.kpfu.ru>
2021-04-05 01:17:32 +0300

    Create MyList project

---
+++ MyList/MyList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyList
+{
+    class MyList<T>
+    { 
+        public T[] Mas { get; set; }
+
+
+        public MyList()
+        {
+            Mas = new T[1];
+            int Length = Mas.Length;
+        }
+
+        public MyList(T[] mas)
+        {
+            Mas = mas;
+            int Length = Mas.Length;
+        }
+
+        public void Add(T item)
+        {
+            int last = Mas.Length;
+            Increase();
+            Mas[last] = item;
+        }
+
+        public void AddRange(T[] collection)
+        {
+            var res = new T[Mas.Length + collection.Length];
+            int indexCollect = 0;
+            for (int i = 0; i < res.Length; i++)
+            {
+                if (i < Mas.Length)
+                {
+                    res[i] = Mas[i];
+                }
+                else
+                {
+                    res[i] = collection[indexCollect];
+                    indexCollect++;
+                }
+            }
+            Mas = res;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Mas.Length; i++)
+            {
+                if (Mas[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            var res = new T[Mas.Length + 1];
+            int indexMas = 0;
+            for (int i = 0; i < res.Length; i++)
+            {
+                if (i == index)
+                {
+                    res[i] = item;
+                }
+                else
+                {
+                    res[i] = Mas[indexMas];
+                    indexMas++;
+                }
+            }
+            Mas = res;
+        }
+
+        public bool Remove(T item)
+        {
+            int countItem = 0;
+            for (int i = 0; i < Mas.Length; i++)
+            {
+                if (Mas[i].Equals(item))
+                    countItem++;
+            }
+
+            var res = new T[Mas.Length - countItem];
+
+            int indexRes = 0;
+            for (int i = 0; i < Mas.Length; i++)
+            {
+                if (Mas[i].Equals(item))
+                {
+                    countItem--;
+                    continue;
+                }
+                else
+                {
+                    res[indexRes] = Mas[i];
+                    indexRes++;
+                }
+            }
+            Mas = res;
+            if (countItem == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            var res = new T[Mas.Length - 1];
+
+            int indexRes = 0;
+            for (int i = 0; i < Mas.Length; i++)
+            {
+                if (i == index)
+                {
+                    continue;
+                }
+                res[indexRes] = Mas[i];
+                indexRes++;
+            }
+            Mas = res;
+        }
+
+        public void Sort()
+        {
+            Array.Sort<T>(Mas, 0, Mas.Length);
+        }
+
+        private void Increase()
+        {
+
+            var increace = new T[Mas.Length +1];
+                for (int i = 0; i < Mas.Length; i++)
+                {
+                    increace[i] = Mas[i];
+                }
+            Mas = increace;
+        }
+
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 3, 4, 45, 6, 34 };
+            int[] b = new int[] { 45, 5 , 64};
+
+            MyList<int> test1 = new MyList<int>(a);
+            MyList<int> test2 = new MyList<int>(a);
+
+            test1.Add(8);
+
+            Console.WriteLine("Прибавить число");
+            foreach (var res in test1.Mas)
+            {
+                Console.Write(res + " ");
+            }
+
+            test2.AddRange(b);
+
+            Console.WriteLine("\nПрибавить массив");
+            foreach (var res in test2.Mas)
+            {
+                Console.Write(res + " ");
+            }
+
+            Console.WriteLine("\nУзнать индекс");
+            Console.WriteLine("\n"+test2.IndexOf(45));
+            Console.WriteLine(test2.IndexOf(12));
+
+
+            test2.Insert(4, 1337);
+            Console.WriteLine("\nВставить число на 1337 на 4 индекс");
+            foreach (var res in test2.Mas)
+            {
+                Console.Write(res + " ");
+            }
+
+            test2.Remove(45);
+            Console.WriteLine("\nУдалить элемент 45");
+            foreach (var res in test2.Mas)
+            {
+                Console.Write(res + " ");
+            }
+
+            test2.RemoveAt(3);
+            Console.WriteLine("\nУдалить элемент 3-го индекса");
+            foreach (var res in test2.Mas)
+            {
+                Console.Write(res + " ");
+            }
+
+            test2.Sort();
+            Console.WriteLine("\nСортировка");
+            foreach (var res in test2.Mas)
+            {
+                Console.Write(res + " ");
+            }
+
+        }
+
+
+    }
+}
